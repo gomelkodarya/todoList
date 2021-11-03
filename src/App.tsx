9642caacdbb275acde29cf3b9937adc090b9adbe
gomelkodarya <gomelkodarya@gmail.com>
2021-11-03 22:08:21 +0300

    pass props to TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
-import {TodoList} from "./TodoList";
+import {TaskType, TodoList} from "./TodoList";
 
 export const App = () => {
+    const tasks1: Array<TaskType> = [
+        {id: 1, title: 'HTML', isDone: true},
+        {id: 2, title: 'CSS', isDone: true},
+        {id: 3, title: 'JavaScript', isDone: true},
+    ]
+
+    const tasks2: Array<TaskType> = [
+        {id: 1, title: 'Milk', isDone: true},
+        {id: 2, title: 'Apples', isDone: false},
+        {id: 3, title: 'Bread', isDone: false},
+    ]
+
     return (
         <div className="App">
-            <TodoList/>
-            <TodoList/>
-            <TodoList/>
+            <TodoList title={'What to learn'} tasks={tasks1}/>
+            <TodoList title={'What to buy'} tasks={tasks2}/>
         </div>
     );
 }

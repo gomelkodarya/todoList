44074ed0921729aee381975c292833c3db5b2d06
gomelkodarya <gomelkodarya@gmail.com>
2021-11-07 23:07:46 +0300

    add reducer for todoLists and create unit tests

---
+++ src/state/todolists-reducer.ts
@@ -0,0 +1,58 @@
+import {FilterValuesType, TodoListType} from "../App";
+import {v1} from "uuid";
+
+export type RemoveTodoListActionType = {
+    type: 'REMOVE-TODOLIST'
+    id: string
+}
+
+export type AddTodoListActionType = {
+    type: 'ADD-TODOLIST'
+    title: string
+}
+
+export type ChangeTodoListTitleActionType = {
+    type: 'CHANGE-TODOLIST-TITLE'
+    title: string
+    id: string
+}
+
+export type ChangeTodoListFilterActionType = {
+    type: 'CHANGE-TODOLIST-FILTER'
+    filter: FilterValuesType
+    id: string
+}
+
+type ActionsType = RemoveTodoListActionType |
+    AddTodoListActionType |
+    ChangeTodoListTitleActionType |
+    ChangeTodoListFilterActionType
+
+
+
+export const todoListsReducer = (state: Array<TodoListType>, action: ActionsType): Array<TodoListType> => {
+    switch (action.type) {
+        case 'REMOVE-TODOLIST':
+            return state.filter(s => s.id !== action.id)
+        case 'ADD-TODOLIST':
+            return [
+                ...state,
+                {id: v1(), title: action.title, filter: 'all'}
+            ]
+        case 'CHANGE-TODOLIST-TITLE':
+            let todoList = state.find(tl => tl.id === action.id)
+            if(todoList) {
+                todoList.title = action.title
+            }
+            return [...state]
+        case 'CHANGE-TODOLIST-FILTER':
+            let todolist = state.find(tl => tl.id === action.id)
+            if(todolist) {
+                todolist.filter = action.filter
+            }
+            return [...state]
+        default:
+            throw new Error("I don't understand this type")
+    }
+}
+
